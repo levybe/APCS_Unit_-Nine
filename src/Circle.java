c34c24167e9694a7e01e728b6ae2ae3bd0f17e9c
levybe <levybe@ssfs.org>
2022-03-31 15:14:34 -0400

    Worked on Circle a little.

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,25 @@
-public class Circle {
+public class Circle extends Shape {
+    private double radius;
 
+    public Circle () {
+        super();
+        radius = 10.0;
+    }
+
+    public Circle (String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius () {
+        return radius;
+    }
+
+    public void setRadius (double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea () {
+
+    }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -2,16 +2,29 @@ public class Triangle extends Shape {
     private double side1, side2, side3;
 
     public Triangle () {
+        super();
         side1 = 2.0;
         side2 = 2.0;
         side3 = 2.0;
     }
 
     public Triangle (double side1, double side2, double side3, String color, boolean filled) {
+        super(color, filled);
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
-        setColor(color);
-        setFilled(filled);
+    }
+
+    public double getArea() {
+        double s = (side1 + side2 + side3) / 2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    public String toString () {
+        return "The triangle has sides of " + side1 + ", " + side2 + ", and " + side3 + ". \n" + super.toString();
     }
 }
